1c9f7a9e97ef9e1a9b4d37f2f81e3c660dfad9b0
Lucas Farris <lucas@farris.com.br>
2015-06-22 21:07:45 -0300

    Novos dados

--- code/runTest.sh
+++ code/runTest.sh
@@ -33,3 +33,27 @@ python klt.py ../videos/original/f08.mp4 10 200 10 400
 python meanShift.py ../videos/original/f09.mp4 80 140 120 120
 python camShift.py ../videos/original/f09.mp4 80 140 120 120
 python klt.py ../videos/original/f09.mp4 70 10 200 200
+
+python meanShift.py ../videos/original/f10.mp4 280 240 40 30
+python camShift.py ../videos/original/f10.mp4 280 240 40 30
+python klt.py ../videos/original/f10.mp4 200 240 200 200
+
+python meanShift.py ../videos/original/f11.mp4 260 430 60 60
+python camShift.py ../videos/original/f11.mp4 260 430 60 60
+python klt.py ../videos/original/f11.mp4 300 240 200 200
+
+python meanShift.py ../videos/original/f12.mp4 240 300 40 30
+python camShift.py ../videos/original/f12.mp4 240 300 40 30
+python klt.py ../videos/original/f12.mp4 200 230 200 200
+
+python meanShift.py ../videos/original/f13.mp4 240 300 40 30
+python camShift.py ../videos/original/f13.mp4 240 300 40 30
+python klt.py ../videos/original/f13.mp4 200 230 200 200
+
+python meanShift.py ../videos/original/f14.mp4 130 420 30
+python camShift.py ../videos/original/f14.mp4 130 420 30
+python klt.py ../videos/original/f14.mp4 100 100 100 300
+
+python meanShift.py ../videos/original/f15.mp4 180 900 80 80
+python camShift.py ../videos/original/f15.mp4 180 900 80 80
+python klt.py ../videos/original/f15.mp4 100 200 100 1000
